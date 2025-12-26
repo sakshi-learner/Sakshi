3179ba2ff6365d115578bde74f0d3f4458fa2aa4
sakshi-learner <144124963+sakshi-learner@users.noreply.github.com>
2025-12-26 13:58:08 +0530

    resume button add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import {Sparkles} from 'lucide-react'
+import React from 'react';
+import { Sparkles , Download} from 'lucide-react';
+
 function Header() {
   return (
-    <div className='p-5 bg-gray-950 flex text-white sticky top-0 border-b border-teal-800 transition-all duration-300
-  hover:shadow-lg hover:shadow-teal-500/40 '>
-      <h1 className='text-teal-500 hover:shadow-xl hover:scale-105'>
+    <div className='p-5 bg-gray-950 flex justify-between items-center text-white sticky top-0 border-b border-teal-800 transition-all duration-300 hover:shadow-lg hover:shadow-teal-500/40'>
+      
+      <h1 className='flex items-center gap-2 text-teal-500 hover:shadow-xl hover:scale-105'>
         <Sparkles 
-        color="#14B8A6" 
-        count={10} 
-        minSize={5} 
-        maxSize={10} 
-        overflowPx={0} 
+          color="#14B8A6" 
+          count={10} 
+          minSize={5} 
+          maxSize={10} 
+          overflowPx={0} 
         />
+        Sakshi
       </h1>
-      <h1>Sakshi</h1>
+
+      {/* Resume download button */}
+      <a
+        href="/Sakshi/ResumeSakshisingh_.pdf"  // file must be in public folder
+        download
+        className='flex items-center gap-2 bg-teal-500 hover:bg-teal-600 text-black font-semibold py-2 px-4 rounded'
+      >
+        Resume    <Download />
+      </a>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
